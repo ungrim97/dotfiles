538e4b14c5f133341381e3137c1286c4b791c663
Mike Eve <ungrim97@gmail.com>
2017-07-25 06:56:26 +0100

    Move config to correct dir path

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -5,4 +5,5 @@ antigen bundle Tarrasch/zsh-colors
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
 antigen apply
